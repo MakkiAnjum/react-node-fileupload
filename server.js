d3db65b8be36d32e140ce1298b2e88f487d50bd9
MakkiAnjum <muhammadmakkianjum@yahoo.om>
2019-01-29 09:42:08 +0500

    single file upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const cors = require('cors');
+
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'public');
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage }).single('file');
+
+app.post('/upload', (req, res) => {
+  upload(req, res, function(err) {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err);
+    } else if (err) {
+      return res.status(500).json(err);
+    }
+    return res.status(200).send(req.file);
+  });
+});
+
+app.listen(8000, () => console.log('app is listening on port 8000'));
---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Progress } from 'reactstrap';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null
+    };
+  }
+
+  onChangeHandler = e => {
+    this.setState({ selectedFile: e.target.files[0], loaded: 0 });
+  };
+
+  onClickHandler = () => {
+    const data = new FormData();
+    data.append('file', this.state.selectedFile);
+    axios
+      .post('http://localhost:8000/upload', data, {
+        onUploadProgress: ProgressEvent => {
+          this.setState({
+            loaded: (ProgressEvent.loaded / ProgressEvent.total) * 100
+          });
+        }
+      })
+      .then(res => {
+        console.log(res);
+      });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="offset-md-3 col-md-6">
+            <div className="form-group files">
+              <label>Upload your file</label>
+              <input
+                type="file"
+                name="file"
+                onChange={this.onChangeHandler}
+                className="form-control"
+                multiple=""
+              />
+            </div>
+            <div className="form-group">
+              <Progress max="100" color="success" value={this.state.loaded}>
+                {Math.round(this.state.loaded, 2)}%
+              </Progress>
+            </div>
+            <button
+              type="button"
+              className="btn btn-primary btn-block"
+              onClick={this.onClickHandler}
+            >
+              Upload
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
